98d9e99509758b9dfdc32be88868ef54ffa276b7
iuser <i@iuser.link>
2022-08-19 16:44:01 +0800

    .

--- pg/deploy.sh
+++ pg/deploy.sh
@@ -1,1 +1,9 @@
-../git/deploy.sh
+#!/usr/bin/env bash
+
+DIR=$(dirname $(realpath "$0"))
+cd $DIR
+set -ex
+
+NAME=$(cat fly.toml|tomlq -r .app)
+flyctl secrets import --app $NAME < ./env || true
+fly deploy
